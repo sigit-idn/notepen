d4a6a010c4d385db2f3dff62da4d828e9c0832d5
sigit <sigit.idn@gmail.com>
2021-05-29 21:11:37 +0900

    Install react-boostrap and react router dom, add register, login, and dashboard page

--- src/containers/page/App/index.js
+++ src/containers/page/App/index.js
@@ -1,22 +1,43 @@
 import './App.css';
+import {BrowserRouter as Router, Route, Link, Switch} from 'react-router-dom';
+import Dashboard from '../Dashboard';
+import Login from '../Login';
+import Register from '../Register';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/login">Login</Link>
+            </li>
+            <li>
+              <Link to="/register">Register</Link>
+            </li>
+          </ul>
+        </nav>
+
+        {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+        <Switch>
+          <Route path="/" exact>
+            <Dashboard />
+          </Route>
+          <Route path="/login">
+            <Login />
+          </Route>
+          <Route path="/register">
+            <Register />
+          </Route>
+        </Switch>
+      </div>
+
+    </Router>
   );
 }
 
---
+++ src/containers/page/Dashboard/index.js
@@ -0,0 +1,11 @@
+const Dashboard = () => {
+    return (
+        <div>
+            <h2>Dashboard Page</h2>
+            <button>Go To Register</button>
+            <button>Go To Dashboard</button>
+        </div>
+    )
+}
+
+export default Dashboard;
